69abd9aa841e14a443453eda4f2471408f968db3
Jose Fernandez (magec) <joseferper@gmail.com>
2025-08-14 16:37:22 +0200

    Wip

--- preview/src/main.ts
+++ preview/src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
 
 /**
  * The main function for the action.
@@ -8,15 +7,10 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const services: string = core.getInput('services')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Services ${services} milliseconds ...`)
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
